3aaba9fe8049193aae1c83bb2a78f9698c10231e
ismailatici <ismailatici@aol.com>
2022-04-12 01:01:51 +0200

    inheritance car BMW Tesla

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,9 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota=new Toyota("Toyota","Camry",2020,"gary",123456);
+        Tesla tesla=new Tesla("Tesla","model S",2010,"blue",23900,123456);
+        BMW bmw=new BMW("BMW","x3",2000,"siyah",34000,590);
+    }
+}
--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -3,7 +3,7 @@ package day38_Inheritance.carTask;
 import day38_Inheritance.Car;
 
 public class Toyota extends Car {
-    public Toyota(String brand, String model, int year, String color, double price, int miles) {
+    public Toyota(String brand, String model, int year, String color, double price) {
         super("Toyota", model, year, color, price, miles);
     }
     public void reliable(){
