943631d78b2efe590074584e969d9af3d0c07c99
ismailatici <ismailatici@aol.com>
2022-04-11 23:02:35 +0200

    inheritance car

---
+++ src/day38_Inheritance/Car.java
@@ -0,0 +1,38 @@
+package day38_Inheritance;
+
+public class Car {
+    public String brand,model;
+    public int year;
+    public String color;
+    public double price;
+    public int miles;
+
+    public Car(String brand, String model, int year, String color, double price, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.miles = miles;
+    }
+
+    public void drive(){
+        System.out.println(brand + " " + model+"is driving");
+    }
+
+    public void start(){
+        System.out.println(brand+" "+model+"is starting");
+
+    }
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                ", miles=" + miles +
+                '}';
+    }
+}
