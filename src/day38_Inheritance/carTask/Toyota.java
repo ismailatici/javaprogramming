d68518973818f49ffb8709693acb791120cdae6d
ismailatici <ismailatici@aol.com>
2022-04-11 23:17:07 +0200

    inheritance car

---
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.carTask;
+
+import day38_Inheritance.Car;
+
+public class Toyota extends Car {
+    public Toyota(String brand, String model, int year, String color, double price, int miles) {
+        super("Toyota", model, year, color, price, miles);
+    }
+    public void reliable(){
+        System.out.println(brand+" "+model+"is reliable");
+
+    }
+}
